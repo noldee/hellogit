98084d1c7db7fc57d30a68f7d4a663f183eefc51
Walter Soca <188809273+noldee@users.noreply.github.com>
2026-01-02 10:20:55 -0500

    MySQL para epxress y organizacion de carpetas

--- hellogit.js
+++ hellogit.js
@@ -6,11 +6,16 @@ app.use("/static", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+// 404 — EXPRESS 5
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(port, () => {
---
+++ models/ModelPrueba.js
@@ -0,0 +1,7 @@
+const db = require("../util/database");
+
+module.exports = class ModelPrueba {
+    constructor(){
+        
+    }
+};
